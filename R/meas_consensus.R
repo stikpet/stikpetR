050d2ae9be03bc3c738f263e70af4ce88a9651c9
Peter Stikker <stikpet@gmail.com>
2024-08-06 16:46:45 +0200

    adjusted consensus measure

--- R/meas_consensus.R
+++ R/meas_consensus.R
@@ -10,15 +10,14 @@
 #' 
 #' @details 
 #' The formula used (Tastle et al., 2005, p. 98):
-#' \deqn{\text{Cns}\left(X\right) = 1 + \sum_{i=1}^k p_i \log_2\left(1 - \frac{\left|X_i - \mu_X\right|}{d_X}\right)}
+#' \deqn{\text{Cns}\left(X\right) = 1 + \sum_{i=1}^k p_i \log_2\left(1 - \frac{\left|i - \mu_X\right|}{d_X}\right)}
 #' With:
-#' \deqn{\mu_X = \frac{\sum_{i=1}^k X_i\times F_i}{n}}
-#' \deqn{d_X = \max\left(X_i\right) - \min\left(X_i\right)}
+#' \deqn{\mu_X = \frac{\sum_{i=1}^k i\times F_i}{n}}
+#' \deqn{d_X = k - 1}
 #' \deqn{p_i = \frac{F_i}{n}}
 #' 
 #' *Symbols used:*
 #' \itemize{
-#' \item \eqn{X_i} the rank of category i
 #' \item \eqn{F_i} the frequency (count) of the i-th category (after they have been sorted)
 #' \item \eqn{n} the sample size
 #' \item \eqn{k} the number of categories.
@@ -34,18 +33,6 @@
 #' @author 
 #' P. Stikker. [Companion Website](https://PeterStatistics.com), [YouTube Channel](https://www.youtube.com/stikpet), [Patreon donations](https://www.patreon.com/bePatron?u=19398076)
 #' 
-#' @examples 
-#' file2 = 'https://peterstatistics.com/Packages/ExampleData/StudentStatistics.csv'
-#' df2 = read.csv(file2, sep=';', na.strings=c("", "NA"))
-#' #Example 1: Text dataframe
-#' ex1 = df2[['Teach_Motivate']]
-#' order = c("Fully Disagree", "Disagree", "Neither disagree nor agree", "Agree", "Fully agree")
-#' me_consensus(ex1, levels=order)
-#' 
-#' #Example 2: Numeric data
-#' ex2 = c(1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5)
-#' me_consensus(ex2) 
-#' 
 #' @export
 me_consensus <- function(data, levels=NULL){
   
@@ -58,10 +45,12 @@ me_consensus <- function(data, levels=NULL){
   
   freq = table(dataN)
   n = sum(freq)
+  k = length(freq)
   p = freq/n
-  r = seq(length(freq))
-  m = sum(freq*r/n)
-  d = max(r) - min(r)
+  r = seq(k)
+  m = sum(freq*r)/n
+  
+  d = k - 1
   
   cns = 1 + sum(p*log(1 - abs(r - m)/d, base=2))
   
